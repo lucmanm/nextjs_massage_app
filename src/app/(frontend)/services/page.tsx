09600396e820dee1f985876160d86ce2b875b3c6
lucmanm <greenjamez22@gmail.com>
2025-01-25 01:13:48 +0300

    update changes

--- src/app/(frontend)/services/page.tsx
+++ src/app/(frontend)/services/page.tsx
@@ -2,9 +2,9 @@ import GallerySection from "@/components/gallery-section";
 
 export default function Services() {
   return (
-    <div className="container mx-auto px-4 py-12">
+    <div className="container mx-auto max-sm:px-4 py-12">
       <h1 className="text-4xl font-bold mb-6">Our Services</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div>
         <GallerySection />
       </div>
     </div>
--- src/components/gallery-section.tsx
+++ src/components/gallery-section.tsx
@@ -71,9 +71,9 @@ export default function GallerySection() {
   ];
 
   return (
-    <section className="px-4 py-8 lg:py-16">
+    <section className="py-8 lg:py-16">
       <h2 className="text-3xl md:text-4xl font-bold text-center mb-8">Our Exquisite Gallery</h2>
-      <div className="grid max-sm:grid-cols-2 lg:grid-cols-4 gap-6">
+      <div className="grid max-sm:grid-cols-2 lg:grid-cols-4 gap-6 max-sm:gap-3 ">
         {galleryItems.map((item, index) => (
           <div
             key={index}
@@ -84,12 +84,12 @@ export default function GallerySection() {
 
               {/* Price */}
               <div className="absolute bottom-2 right-2 bg-white rounded-full px-4">
-                <span className="text-lg font-semibold mb-2">{item.price} SAR</span>
+                <span className="text-lg font-semibold mb-2 text-blue-600">{item.price} SAR</span>
               </div>
             </div>
             <div className="flex flex-col flex-grow p-4">
               <h3 className="text-lg font-semibold mb-2">{item.title}</h3>
-              <p className="text-lg font-semibold text-gray-600 flex-grow ">{item.duration}</p>
+              <p className="text-lg font-semibold text-blue-600 flex-grow ">{item.duration}</p>
               <p className="text-sm text-gray-600 flex-grow">{item.description}</p>
             </div>
             <div className="p-4">
