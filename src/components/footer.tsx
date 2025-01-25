33723b426fca21bf9bca399c1f15bd2b5eb83301
lucmanm <greenjamez22@gmail.com>
2025-01-26 00:17:11 +0300

    Applied: Login Form

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
       <footer className="bg-primary text-primary-foreground py-6">
         <div className="container mx-auto px-4 text-center">
-          <p>&copy; 2023 LucmanmTouch Massage. All rights reserved.</p>
+          <p>&copy; 2023 Touch Massage. All rights reserved.</p>
         </div>
       </footer>
     )
--- src/hook/useMenuStore.ts
+++ src/hook/useMenuStore.ts
@@ -2,14 +2,12 @@ import { create } from "zustand";
 
 interface MenuState {
     isOpen: boolean;
-    openMenu: () => void;
     closeMenu: () => void;
     toggleMenu: () => void;
 }
 
 export const useMenuStore = create<MenuState>((set) => ({
     isOpen: false,
-    openMenu: () => set({ isOpen: true }),
     closeMenu: () => set({ isOpen: false }),
     toggleMenu: () => set((state) => ({ isOpen: !state.isOpen })),
 }));
