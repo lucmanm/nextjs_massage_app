83a6cedd52a806846497da37a9f76ee1c97942ad
lucmanm <greenjamez22@gmail.com>
2025-01-28 23:45:46 +0300

    Update: Group layout for forntend and backend

---
+++ src/app/(backend)/dashboard/page.tsx
@@ -0,0 +1,21 @@
+import ConnectWithUs from "@/components/connect-with-us";
+import FeaturesSection from "@/components/features-section";
+import GallerySection from "@/components/gallery-section";
+import HeroSection from "@/components/hero-section";
+import Testimonials from "@/components/testimonials";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="container mx-auto py-8 space-y-8 max-sm:px-2">
+        <HeroSection />
+        <FeaturesSection />
+        <GallerySection />
+      </div>
+      <div className="py-16 px-4 bg-gradient-to-b from-blue-100 to-blue-50 space-y-8">
+        <Testimonials />
+        <ConnectWithUs />
+      </div>
+    </div>
+  );
+}
