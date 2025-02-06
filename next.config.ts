cd306e6654485d7843d79bb0e536e5d760213015
lucmanm@icloud.com <lucmanm@icloud.com>
2025-02-07 02:44:13 +0300

    update: config update for using other images

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,13 @@ const nextConfig: NextConfig = {
         pathname: '/**',
         search: '',
       },
+      {
+        protocol: 'https',
+        hostname: 'api.deepai.org',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
     ],
   },
 }
