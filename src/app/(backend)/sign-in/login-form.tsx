c2b289259867928b187fff78b789400af02f124a
lucmanm@icloud.com <lucmanm@icloud.com>
2025-02-09 00:57:24 +0300

    update and changes

--- src/app/(backend)/sign-in/login-form.tsx
+++ src/app/(backend)/sign-in/login-form.tsx
@@ -1,17 +1,20 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import ButtonCustomized from "@/components/button-customized";
+import { CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
 import { loginSchema } from "@/lib/zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { LoaderPinwheel, Lock, Save, User } from "lucide-react";
+import { Lock, LogIn, User } from "lucide-react";
 import { signIn } from "next-auth/react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-export function AdminSignInForm() {
+export function AdminSignInForm({ ...props }: React.ComponentPropsWithoutRef<"div">) {
   const router = useRouter();
 
   const form = useForm<z.infer<typeof loginSchema>>({
@@ -37,65 +40,102 @@ export function AdminSignInForm() {
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-md">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <div className="relative">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <User className="size-4 text-gray-500" /> {/* Replace User with your desired icon */}
-                </div>
-                <FormControl>
-                  <Input
-                    placeholder="Username"
-                    {...field}
-                    className="pl-10" // Add padding to the left to make space for the icon
-                  />
-                </FormControl>
+    <div className={cn("flex flex-col gap-6", props.className)} {...props}>
+      {/* <Card> */}
+      <CardHeader className="text-center">
+        <CardTitle className="text-xl">Admin</CardTitle>
+        <CardDescription>Please enter your auth Account</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="grid gap-6">
+{/*
+              <div className="grid grid-cols-2 gap-4">
+                <Button variant="outline" className="w-full">
+                  <AppleIcon />
+                  Login with Apple
+                </Button>
+                <Button variant="outline" className="w-full bg-blue-600 text-white">
+                  <GoogleIcon />
+                  Login with Google
+                </Button>
               </div>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <div className="relative">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <Lock className="size-4 text-gray-500" /> {/* Replace Lock with your desired icon */}
-                </div>
-                <FormControl>
-                  <Input
-                    type="password"
-                    placeholder="Password"
-                    {...field}
-                    className="pl-10" // Add padding to the left to make space for the icon
+              <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                <span className="relative z-10 bg-background px-2 text-muted-foreground">Or continue with</span>
+              </div> */}
+
+              <div className="grid gap-6">
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <div className="relative">
+                          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <User className="size-4 text-gray-500" /> {/* Replace User with your desired icon */}
+                          </div>
+                          <FormControl>
+                            <Input
+                              placeholder="Username"
+                              {...field}
+                              className="pl-10" // Add padding to the left to make space for the icon
+                            />
+                          </FormControl>
+                        </div>
+                        <FormMessage />
+                      </FormItem>
+                    )}
                   />
-                </FormControl>
+                </div>
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <div className="flex items-center">
+                          <FormLabel>Password</FormLabel>
+                          {/* <Link href="#" className="ml-auto text-sm underline-offset-4 hover:underline">
+                            Forgot your password?
+                          </Link> */}
+                        </div>
+                        <div className="relative">
+                          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <Lock className="size-4 text-gray-500" /> {/* Replace Lock with your desired icon */}
+                          </div>
+                          <FormControl>
+                            <Input
+                              type="password"
+                              placeholder="Password"
+                              {...field}
+                              className="pl-10" // Add padding to the left to make space for the icon
+                            />
+                          </FormControl>
+                        </div>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <ButtonCustomized title="Login" formState={form.formState.isSubmitting} icon={LogIn} />
               </div>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit" className="w-full font-medium" disabled={form.formState.isSubmitting}>
-          {form.formState.isSubmitting ? (
-            <>
-              <LoaderPinwheel className="animate-spin" /> Loading...
-            </>
-          ) : (
-            <>
-              <Save /> Login
-            </>
-          )}
-        </Button>
-      </form>
-    </Form>
+              {/* <div className="text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <a href="#" className="underline underline-offset-4">
+                  Sign up
+                </a>
+              </div> */}
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+      {/* </Card> */}
+      {/* <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  /">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+      </div> */}
+    </div>
   );
 }
--- src/app/(backend)/sign-in/page.tsx
+++ src/app/(backend)/sign-in/page.tsx
@@ -1,5 +1,3 @@
-import { GalleryVerticalEnd } from "lucide-react";
-
 import { auth } from "@/auth";
 import { imagesPlaceHolder } from "@/constant/data";
 import Image from "next/image";
@@ -12,20 +10,8 @@ export default async function LoginPage() {
 
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Touch Massage
-          </a>
-        </div>
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
-            <AdminSignInForm />
-          </div>
-        </div>
+      <div className="flex items-center justify-center flex-col gap-4 p-6 md:p-10">
+        <AdminSignInForm className="md:w-1/2" />
       </div>
       <div className="relative hidden bg-muted lg:block">
         <Image fill src={imagesPlaceHolder} alt="image default" />
