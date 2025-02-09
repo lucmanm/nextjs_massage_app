0a1c1f488c202bd06c1c28a4312b9a31c1b00432
lucmanm@icloud.com <lucmanm@icloud.com>
2025-02-09 16:56:23 +0300

    update and changes

--- src/app/(backend)/admin/dashboard/page.tsx
+++ src/app/(backend)/admin/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { Users, UserPlus, DollarSign, Activity } from "lucide-react";
 
 export default function Dashboard() {
   return (
-    <div className="min-h-screen p-4">
+    <div className="min-h-screen p-4 w-full">
       {/* Top Cards */}
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4 *:cursor-pointer">
         <CardStats title="Visitors" value="1,234" icon={Activity} description="7% increase from last week" />
