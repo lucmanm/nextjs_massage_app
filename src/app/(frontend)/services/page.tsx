79bd820936ccc1fefebfb4fb9f400149e4c90d54
lucmanm@icloud.com <lucmanm@icloud.com>
2025-02-09 16:53:25 +0300

    update and changes

--- src/app/(frontend)/services/page.tsx
+++ src/app/(frontend)/services/page.tsx
@@ -2,8 +2,8 @@ import GallerySection from "@/components/gallery-section";
 
 export default function Services() {
   return (
-    <div className="container mx-auto max-sm:px-4 py-12">
-      <h1 className="text-4xl font-bold mb-6">Our Services</h1>
+    <div className="container mx-auto max-sm:px-4 py-4">
+      {/* <h1 className="text-4xl font-bold mb-6">Our Services</h1> */}
       <div>
         <GallerySection />
       </div>
